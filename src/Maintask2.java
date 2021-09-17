d1343c4178704568e47601b89bda4bdf68421abf
Lolliagodx <ilyavas03@gmail.com>
2021-09-17 17:20:33 +0300

    second task

--- src/Maintask2.java
+++ src/Maintask2.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Maintask2 {
     public static void main(String[] args) {
-        
+        Scanner vvod = new Scanner(System.in);
+        System.out.println("Введите значение, которое хотите перевернуть:");
+        String znach = vvod.nextLine();
+        StringBuilder reverse = new StringBuilder(znach);
+       System.out.println ("Перевернутое значение:" + " " + reverse.reverse());
+
+
     }
 }
