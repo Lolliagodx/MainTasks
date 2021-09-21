f3b923cd406e8dfcc3ed48a370187f6f4c3270f0
Lolliagodx <ilyavas03@gmail.com>
2021-09-21 19:37:58 +0300

    1st optional task complited

--- src/Optionaltask1.java
+++ src/Optionaltask1.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Optionaltask1 {
     public static void main(String[] args) {
-//        Scanner enter = new Scanner(System.in);
-//        String numbers = enter.nextLine();
-//        String Long = null, Short = null;
-        int[] numbers = {12, 4356, 100000, 245};
-        for (int result:numbers) {
+        Scanner enter = new Scanner(System.in);
+        String numbers = enter.nextLine();
+        String longWord = " ", shortWord = numbers;
 
+        for (String s : numbers.split(" ")) {
+            if (s.length() > longWord.length()) longWord = s;
+            if (s.length() < shortWord.length()) shortWord = s;
         }
-
-
-
-
-
-
+        System.out.println("Самое длинное слово: " + longWord );
+        System.out.println("Длина: " + longWord.length());
+        System.out.println("Самое короткое слово: " + shortWord);
+        System.out.println("Длина: " + shortWord.length());
     }
 }
